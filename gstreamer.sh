f50885ecab9a1f56379fbbaf01071499d60a3d2a
Newton Kim <babynewton73@gmail.com>
2014-06-02 13:58:27 +0900

    gstreamer.sh has been introduced

---
+++ gstreamer.sh
@@ -0,0 +1,27 @@
+set -e
+DISTRIB_CODENAME=$(grep -oP 'CODENAME=(.+)' -m 1 /etc/lsb-release | awk -F= '{ print $NF }')
+GSTREAMER_REPO_LINK="deb http://ppa.launchpad.net/gstreamer-developers/ppa/ubuntu"
+GSTREAMER_SRC_REPO_LINK="deb-src http://ppa.launchpad.net/gstreamer-developers/ppa/ubuntu"
+FULL_GSTREAMER_REPO_LINK="$GSTREAMER_REPO_LINK $DISTRIB_CODENAME main"
+FULL_GSTREAMER_SRC_REPO_LINK="$GSTREAMER_SRC_REPO_LINK $DISTRIB_CODENAME main"
+GSTREAMER="gstreamer1.0*"
+
+if ! grep --quiet "$FULL_GSTREAMER_REPO_LINK" /etc/apt/sources.list; then
+	echo "Adding gstreamer repo to /etc/apt/sources.list"
+	sudo sed -i "\$i$FULL_GSTREAMER_REPO_LINK" /etc/apt/sources.list
+	UPDATE_SOURCES=true
+fi
+
+if ! grep --quiet "$FULL_GSTREAMER_SRC_REPO_LINK" /etc/apt/sources.list; then
+	echo "Adding gstreamer source repo to /etc/apt/sources.list"
+	sudo sed -i "\$i$FULL_GSTREAMER_SRC_REPO_LINK" /etc/apt/sources.list
+	UPDATE_SOURCES=true
+fi
+
+if $UPDATE_SOURCES; then
+	echo "Updating repositories"
+	sudo apt-get update
+fi
+
+echo "installing gstreamer"
+sudo apt-get install ${GSTREAMER}
